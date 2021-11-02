6dd6bf07a063caa3cddd4f8d20d11794e6e00a62
James Noleto Brandao da Silva <muricijames@gmail.com>
2021-11-02 14:42:25 -0300

    Espaçamento

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -12,7 +12,9 @@ namespace Backoffice.Services
         public static string GenerateToken(User user)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
+
             var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
@@ -20,11 +22,16 @@ namespace Backoffice.Services
                     new Claim(ClaimTypes.Name, user.Username.ToString()),
                     new Claim(ClaimTypes.Role, user.Role.ToString())
                 }),
+
                 Expires = DateTime.UtcNow.AddHours(2),
+
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
+
             var token = tokenHandler.CreateToken(tokenDescriptor);
+
             return tokenHandler.WriteToken(token);
+            
         }
     }
 }
